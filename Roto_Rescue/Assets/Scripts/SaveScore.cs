12cb6fada1e187e22ff186692848cdc20bcd23a6
Pablo Vazquez <95883542+pablo123w@users.noreply.github.com>
2022-10-12 11:17:14 -0700

    started my save script
    started the framework of my save script

--- Roto_Rescue/Assets/Scripts/SaveScore.cs
+++ Roto_Rescue/Assets/Scripts/SaveScore.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class SaveScore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health;
+    public float score;
+    public LevelProgression lbl;
+   
+    public void Start()
     {
-        
+       
+       
+    }
+    public void Update()
+    {
+       // score = lbl.ScorePercent;
+    }
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+        PlayerData player = new PlayerData();
+       // player.score = score;
+
+        player.currentLevel = SceneManager.GetActiveScene();
+        bf.Serialize(file, player);
+        file.Close();
+        Debug.Log("saved");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Load()
     {
-        
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData player = (PlayerData)bf.Deserialize(file);
+            SceneManager.LoadScene(player.currentLevel.buildIndex);
+            file.Close();
+            //score = player.score;
+            
+        }
+        else
+        {
+            Debug.Log("NOOOOOOOOOOOOO");
+        }
+    }
+    public void saveButton()
+    {
+        Save();
+    }
+    public void loadButton()
+    {
+        Load();
+    }
+
+    [System.Serializable]
+    class PlayerData
+    {
+        public float score;
+        public Scene currentLevel;
     }
 }
+
