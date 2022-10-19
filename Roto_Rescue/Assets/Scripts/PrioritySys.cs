fcf22ee12bf498f411f4c37df9701c9e023aa023
Pablo Vazquez <95883542+pablo123w@users.noreply.github.com>
2022-10-19 10:03:35 -0700

    added the priority system
    for the claw

---
+++ Roto_Rescue/Assets/Scripts/PrioritySys.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrioritySys : MonoBehaviour
+{
+    public int Priority;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "C_Goober")
+        {
+            Priority = 1;
+            Debug.Log("P = " + Priority);
+        }
+        else if(other.gameObject.tag == "Pickupable")
+        {
+            Priority = 2;
+            Debug.Log("P = " + Priority);
+        }
+    }
+
+}
