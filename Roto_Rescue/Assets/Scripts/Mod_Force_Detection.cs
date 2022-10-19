f9802942b7e0b8e79a046c53afa77c6128570d41
Jon <jonmills3000@gmail.com>
2022-10-19 16:40:56 -0700

    Merge

---
+++ Roto_Rescue/Assets/Scripts/Mod_Force_Detection.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mod_Force_Detection : MonoBehaviour
+{
+    public Rigidbody rg;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+        rg.useGravity = false;
+        rg.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.relativeVelocity.magnitude >= force)
+        {
+            rg.useGravity = true;
+            rg.isKinematic = false;
+        }
+    }
+}
